ca28938351908559088386c78ccc7a487b35c5cf
https://github.com/AND1ofthewrld/DeminTGBot.git <pukitaa@bk.ru>
2023-09-28 20:04:46 +0300

    ADDS

--- configDE.py
+++ configDE.py
@@ -3,17 +3,18 @@ token = '6534499024:AAGhXpMnyUctJ6iaWDXaqOdKLTOTSL4fysg'
 gif = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Rotating_earth_%28large%29.gif/274px-Rotating_earth_%28large%29.gif'
 def get_random_audio():
 
-    # links_of_audio = ['https://raw.githubusercontent.com/AND1ofthewrld/DeminTGBot/a3dbbadac80d78af7d07fbb89fb8e26cd47d656d/BennyBenassiFeat.Gary%20Go_Cinema.mp3',
-    #                   'https://raw.githubusercontent.com/AND1ofthewrld/DeminTGBot/blob/a3dbbadac80d78af7d07fbb89fb8e26cd47d656d/CalvinHarris%20_Pray.mp3']
-    #
-    # random_link = random.choice(links_of_audio)
-    # return random_link
-    link = 'https://github.com/AND1ofthewrld/DeminTGBot/raw/a3dbbadac80d78af7d07fbb89fb8e26cd47d656d/BennyBenassiFeat.Gary%20Go_Cinema.mp3'
-    return link
+    links_of_audio = ['https://github.com/AND1ofthewrld/TGTaskBot2/raw/master/BennyBenassiFeat.Gary%20Go_Cinema.mp3',
+                      'https://github.com/AND1ofthewrld/TGTaskBot2/raw/master/CalvinHarris%20_Pray.mp3',
+                      'https://github.com/AND1ofthewrld/TGTaskBot2/raw/master/Modestep_Snake.mp3',
+                      'https://github.com/AND1ofthewrld/TGTaskBot2/raw/master/OhLand_Sun.mp3',
+                      'https://github.com/AND1ofthewrld/TGTaskBot2/raw/master/VAMPS%20-%20Wake%20Up%20(supermusic.me).mp3']
+    random_link = random.choice(links_of_audio)
+    return random_link
 def get_random_photo():
-    mass_photo = ['https://lh6.googleusercontent.com/cJVy4vk4etKXoT0lvKDVTgcctVaez6f4Im2EM2U4SPomoK84iwdwDJQivZWksR9vAt09DYIjmin19y5EQDmUzh18dCUdXeac8Ts0YDUPusTI3uZmZeqjwqwQp2EPd_IVElBziV9bUrIruQuWyg',
-                  'https://a.d-cd.net/826fd84s-960.jpg','https://cs7.pikabu.ru/post_img/big/2017/10/25/9/150894277615715407.jpg',
-                  'https://rider-skill.ru/wp-content/uploads/2020/08/mount-fuji-1313x875-1.jpg',
-                  'https://fototips.ru/wp-content/uploads/2018/05/image7.jpg']
+    mass_photo = ['https://github.com/AND1ofthewrld/TGTaskBot2/blob/master/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(1).jpg?raw=true',
+                  'https://github.com/AND1ofthewrld/TGTaskBot2/blob/master/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(2).jpg?raw=true',
+                  'https://github.com/AND1ofthewrld/TGTaskBot2/blob/master/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(3).jpg?raw=true',
+                  'https://github.com/AND1ofthewrld/TGTaskBot2/blob/master/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(4).jpg?raw=true',
+                  'https://github.com/AND1ofthewrld/TGTaskBot2/blob/master/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F.jpg?raw=true']
     random_photo = random.choice(mass_photo)
     return random_photo
